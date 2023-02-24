69d3ec8ad8f2ca0591abdfcb897ff41e3271ccad
Kanthapong <kanthapong062@gmail.com>
2023-02-25 01:10:47 +0700

    commit

---
+++ build/flutter_contacts/generated/source/buildConfig/debug/co/quis/flutter_contacts/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package co.quis.flutter_contacts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "co.quis.flutter_contacts";
+  public static final String BUILD_TYPE = "debug";
+}
